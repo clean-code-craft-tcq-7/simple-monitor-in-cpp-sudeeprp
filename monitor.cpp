f729c77ca7cf68853afa85e71e34abe9b92f57f4
Sudeep <sudeep.rp@gmail.com>
2025-07-15 13:19:22 +0530

    reduce complexity

--- monitor.cpp
+++ monitor.cpp
@@ -14,19 +14,30 @@ void vitalsCheckAttention() {
   }
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
+int temperatureOk(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
     vitalsCheckAttention();
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
+  }
+}
+
+int pulseRateOk(float pulseRate) {
+  if (pulseRate < 60 || pulseRate > 100) {
     cout << "Pulse Rate is out of range!\n";
     vitalsCheckAttention();
     return 0;
-  } else if (spo2 < 90) {
+  }
+}
+
+int spo2Ok(float spo2) {
+  if (spo2 < 90) {
     cout << "Oxygen Saturation out of range!\n";
     vitalsCheckAttention();
     return 0;
   }
-  return 1;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  return temperatureOk(temperature) && pulseRateOk(pulseRate) && spo2Ok(spo2);
 }
