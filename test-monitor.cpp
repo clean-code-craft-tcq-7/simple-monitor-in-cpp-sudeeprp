b95a7150129b0c315510fe281512b8a65cf0d390
Sudeep <sudeep.rp@gmail.com>
2025-08-12 12:25:28 +0530

    add tests to cover functionality

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,26 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
+TEST(Monitor, OkWhenAllVitalsAreNormal) {
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+}
+
+TEST(Monitor, TemperatureHigh) {
+  ASSERT_FALSE(vitalsOk(104, 70, 98));
+}
+
+TEST(Monitor, TemperatureLow) {
+  ASSERT_FALSE(vitalsOk(94, 70, 98));
+}
+
+TEST(Monitor, PulseRateHigh) {
+  ASSERT_FALSE(vitalsOk(98.1, 150, 98));
+}
+
+TEST(Monitor, PulseRateLow) {
+  ASSERT_FALSE(vitalsOk(98.1, 30, 98));
+}
+
+TEST(Monitor, Spo2Low) {
+  ASSERT_FALSE(vitalsOk(98.1, 70, 85));
 }
